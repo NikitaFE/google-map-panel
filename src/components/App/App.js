25259f5a75f0d6e784acafc10980b4dc2f02ab2e
NikitaFE <nikromongerredd@gmail.com>
2020-09-25 23:07:49 +0300

    init: add map

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Map from '../Map';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
